6deaf26d2bc75b3dffe402c147f1324731566eae
Christian Cito <30470476+chrcit@users.noreply.github.com>
2024-11-18 00:28:53 +0100

    fix: add load context

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,23 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { getLoadContext } from "./load-context";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [
+    remixCloudflareDevProxy({ getLoadContext }),
+    remix(),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    resolve: {
+      conditions: ["workerd", "worker", "browser"],
+    },
+  },
+  resolve: {
+    mainFields: ["browser", "module", "main"],
+  },
+  build: {
+    minify: true,
+  },
 });
