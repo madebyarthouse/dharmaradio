7fe6713bcdb50c415e5cc8d4f2424a28359903c7
Christian Cito <30470476+chrcit@users.noreply.github.com>
2024-11-18 01:03:45 +0100

    fix: clean up multiple things

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,7 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { getLoadContext } from "./load-context";
 
 export default defineConfig({
-  plugins: [
-    remixCloudflareDevProxy({
-      getLoadContext,
-    }),
-    remix(),
-    tsconfigPaths(),
-  ],
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
