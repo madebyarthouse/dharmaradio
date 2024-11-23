618fd2ad945dd4c7dd87356462ab8a9bec341e83
Christian Cito <30470476+chrcit@users.noreply.github.com>
2024-11-24 00:56:28 +0100

    Update centers.tsx

--- app/routes/centers.tsx
+++ app/routes/centers.tsx
@@ -13,11 +13,12 @@ import { cacheHeader } from "pretty-cache-header";
 
 export const headers = {
   "Cache-Control": cacheHeader({
-    maxAge: "15min",
-    sMaxage: "3hours",
-    staleWhileRevalidate: "1day",
+    maxAge: "6hours",
+    sMaxage: "24hours",
+    staleWhileRevalidate: "1week",
   }),
 };
+
 export async function loader({ request, context }: LoaderFunctionArgs) {
   const { searchQuery, page, sort, hasSearch } = getRequestParams(request, {
     field: "name",
